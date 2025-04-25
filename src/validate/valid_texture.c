33a0ba1364c410915a2a1537390bd3971658b721
yabi <hin050924@gmail.com>
2025-04-25 16:08:23 +0800

    valid_texture  optimizetion

--- src/validate/valid_texture.c
+++ src/validate/valid_texture.c
@@ -6,30 +6,11 @@
 /*   By: yyan-bin <yyan-bin@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 14:42:29 by yyan-bin          #+#    #+#             */
-/*   Updated: 2025/04/25 15:15:43 by yyan-bin         ###   ########.fr       */
+/*   Updated: 2025/04/25 16:07:43 by yyan-bin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
-
-/**
- * @brief Sets direction texture path.
- * @param data The game data structure.
- * @param dir The direction string to check.
- * @details This function sets the corresponding texture path in 
- * 		the game data structure.
- */
-void	get_direction(t_game *data, char *dir)
-{
-	if (!ft_strncmp(dir, "NO ", 3))
-		data->tex_info.north = ft_strdup(dir + 3);
-	else if (!ft_strncmp(dir, "WE ", 3))
-		data->tex_info.west = ft_strdup(dir + 3);
-	else if (!ft_strncmp(dir, "SO ", 3))
-		data->tex_info.south = ft_strdup(dir + 3);
-	else if (!ft_strncmp(dir, "EA ", 3))
-		data->tex_info.east = ft_strdup(dir + 3);
-}
 
 /**
  * @brief Validates the image paths.
@@ -42,16 +23,15 @@ void	get_direction(t_game *data, char *dir)
  */
 int	valid_image(t_game *data, char **dir)
 {
-	int	i;
-
 	if (ft_arrlen(dir) < 4)
 		return (error_msg(NULL, TEX_MISSING, 1, 0));
 	if (ft_strncmp(dir[0], "NO ", 3) || ft_strncmp(dir[1], "SO ", 3)
 		|| ft_strncmp(dir[2], "WE ", 3)
 		|| ft_strncmp(dir[3], "EA ", 3))
 		return (error_msg(NULL, TEX_INVALID, 1, 0));
-	i = 0;
-	while (dir[i] && i < 4)
-		get_direction(data, dir[i++]);
-	return (0);
+	data->tex_info.north = ft_strdup(dir[0] + 3);
+	data->tex_info.west = ft_strdup(dir[1] + 3);
+	data->tex_info.south = ft_strdup(dir[2] + 3);
+	data->tex_info.east = ft_strdup(dir[3] + 3);
+		return (0);
 }
